490cadce572180bbbd0ee84315158267780a292c
Adam Zvada <zvadaada@gmail.com>
2023-12-25 23:15:48 +0100

    merry christmas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='bg-black'>
       <body className={inter.className}>
         {children}
         <Analytics />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { FaGithub } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
 import { FaTiktok } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa6";
+import { Button } from "@/components/ui/button";
 import { compareDesc, format, parseISO } from 'date-fns'
 import { allBlogs, allRecommendations } from "contentlayer/generated"
 
@@ -17,17 +18,19 @@ export default function Landing() {
 
   return (
     <div className="bg-black text-white flex items-center justify-center">
-        <section className="flex flex-col h-screen justify-between w-full max-w-[3000px] mx-auto min-h-screen">
+        <section className="flex flex-col w-full max-w-[3000px] mx-auto min-h-screen">
           <Header />
-          <Main />
-          <div className="flex justify-center space-x-4 p-4">
-            <button onClick={() => setSelectedSegment('writings')} className={`px-4 py-2 ${selectedSegment === 'writings' ? 'bg-white text-black' : ''}`}>Writings</button>
-            <button onClick={() => setSelectedSegment('recommendations')} className={`px-4 py-2 ${selectedSegment === 'recommendations' ? 'bg-white text-black' : ''}`}>Recommendations</button>
-            <button onClick={() => setSelectedSegment('readings')} className={`px-4 py-2 ${selectedSegment === 'readings' ? 'bg-white text-black' : ''}`}>Readings</button>
+          <div className="flex flex-col items-start justify-center w-1/3 mx-auto">
+            <Main />
+            <div className="flex space-x-4 -m-4 py-6">
+              <Button variant='ghost' onClick={() => setSelectedSegment('writings')} className={`${selectedSegment !== 'writings' ? 'text-gray-500' : 'text-white'} hover:text-white hover:bg-gray-900 text-base tracking-tighter`}>What I Think</Button>
+              <Button variant='ghost' onClick={() => setSelectedSegment('recommendations')} className={`${selectedSegment !== 'recommendations' ? 'text-gray-500' : 'text-white'} hover:text-white hover:bg-gray-900 text-base tracking-tighter`}>What I Like</Button>
+              <Button variant='ghost' onClick={() => setSelectedSegment('readings')} className={`${selectedSegment !== 'readings' ? 'text-gray-500' : 'text-white'} hover:text-white hover:bg-gray-900 text-base tracking-tighter`}>What I Read</Button>
+            </div>
+            {selectedSegment === 'writings' && <Writings />}
+            {selectedSegment === 'recommendations' && <Recommendations />}
+            {selectedSegment === 'readings' && <Readings />}
           </div>
-          {selectedSegment === 'writings' && <Writings />}
-          {selectedSegment === 'recommendations' && <Recommendations />}
-          {selectedSegment === 'readings' && <Readings />}
           <Footer />
       </section>
     </div>
@@ -36,7 +39,7 @@ export default function Landing() {
 
 function Main() {
   return (
-    <div className="flex flex-col items-center justify-center h-full pt-24">
+    <div className="flex flex-col justify-center h-64 py-24">
       <div className="flex flex-col items-start justify-center text-left gap-2">
         <h1 className="text-2xl font-medium tracking-tighter">Adam Zvada</h1>
         <h2 className="text-xl font-small text-gray-500 tracking-tighter">curious human being who loves to build <br/> and think about the future</h2>
@@ -49,12 +52,12 @@ function Writings() {
   const blogs = allBlogs.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
 
   return (
-    <div className="mx-auto">
-      <h1 className="text-xl mb-4 text-left">Writings</h1>
+    <div className="flex-col">
+      {/* <h1 className="text-xl mb-4 text-left">Writings</h1> */}
       <div className="prose dark:prose-invert">
-        {blogs.map((blog) => (
-          <Link href={blog.slug}>
-            <article key={blog._id} className="cursor-pointer py-4 hover:bg-gray-900 border-b border-gray-800 w-full">
+        {blogs.map((blog, index) => (
+          <Link href={blog.slug} key={index}>
+            <article className="cursor-pointer py-4 hover:bg-gray-900 border-b border-gray-800 w-full">
               <div className="flex justify-between items-center pb-1">
                 <h2>{blog.title}</h2>
                 <span className="text-gray-500 tracking-tighter">{format(parseISO(blog.date), 'MMM yy')}</span>
@@ -74,11 +77,11 @@ function Recommendations() {
     <div className="mx-auto">
       <h1 className="text-xl mb-4 text-left">What I like</h1>
       <div className="prose dark:prose-invert">
-        {allRecommendations.map((recommendation) => (
-          <Link href={recommendation.website}>
-            <article key={recommendation._id} className="cursor-pointer py-4 hover:bg-gray-900 border-b border-gray-800 w-full flex">
+        {allRecommendations.map((recommendation, index) => (
+          <Link href={recommendation.website} key={index}>
+            <article className="cursor-pointer py-4 hover:bg-gray-900 border-b border-gray-800 w-full flex">
               <div className="flex">
-                <img src={recommendation.content} alt={recommendation.product} className="w-full h-auto" />
+                <img src={recommendation.content} alt={recommendation.product} width={500} height={500} className="w-full h-auto" />
                 <div className="flex flex-col items-left pb-1">
                   <h2>{recommendation.product}</h2>
                   {recommendation.description && <p className="text-gray-500 text-sm font-light">{recommendation.description}</p>}
@@ -98,9 +101,9 @@ function Readings() {
     <div className="mx-auto">
       <h1 className="text-xl mb-4 text-left">What I like</h1>
       <div className="prose dark:prose-invert">
-        {allRecommendations.map((recommendation) => (
-          <Link href={recommendation.website}>
-            <article key={recommendation._id} className="cursor-pointer py-4 hover:bg-gray-900 border-b border-gray-800 w-full flex">
+        {allRecommendations.map((recommendation, index) => (
+          <Link href={recommendation.website} key={index}>
+            <article className="cursor-pointer py-4 hover:bg-gray-900 border-b border-gray-800 w-full flex">
               <div className="flex">
                 <img src={recommendation.content} alt={recommendation.product} className="w-full h-auto" />
                 <div className="flex flex-col items-left pb-1">
@@ -119,7 +122,7 @@ function Readings() {
 
 function Header() {
   return (
-    <div className="flex items-center w-full justify-between p-6">
+    <div className="sticky top-0 z-50 flex items-center w-full justify-between p-6">
       <Image src="/az.svg" alt="AZ Logo" width={32} height={32} priority/>
       <div className="flex justify-end gap-2 items-center group">
         <span className="font-light text-xs uppercase text-white text-opacity-25 group-hover:translate-x-[-10px] transition-transform md:inline hidden">Adam&apos;s socials -&gt;</span> 
@@ -145,7 +148,7 @@ function Header() {
 
 function Footer() {
   return (
-    <div className="w-full p-6">
+    <div className="sticky bottom-0 z-50 w-full px-6 pb-6 bg-black">
       <div className="flex items-center justify-between border-t border-gray-800 pt-3">
         <div className="text-sm text-white">
           <span>Adam Zvada</span>
